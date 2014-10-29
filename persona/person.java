708ef955055a198f144aaf3a413741eaa4e4e73d
rubben <rubenfc2013@gmail.com>
2014-10-29 12:57:03 +0100

    añadido metodo promt

---
+++ persona/person.java
@@ -0,0 +1,20 @@
+
+/**
+ * Write a description of class person here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class person
+{
+    private int age
+    /**
+     * Constructor for objects of class person
+     */
+    public person(int age)
+    {
+        
+    }
+
+    
+}
